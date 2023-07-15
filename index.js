d26bb8abd16244af682b1861bccbdafd7b5adb9d
vimal336 <asvimal336@gmail.com>
2023-07-15 14:18:18 +0530

    node js practice

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+
+//OS Methods:
 var os = require('os');
-var path = require('path');
 
 console.log(os.uptime());
 console.log(os.type());
 console.log(os.homedir());
 console.log(os.version());
-console.log(path.sep);
+
+
 console.log(__dirname);
 console.log(__filename);
+
+
+const path = require('path');
+console.log(path.sep);
+
+console.log(path.basename(__filename));
+console.log(path.dirname(__dirname));
+console.log(path.extname(__filename));
