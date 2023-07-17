589c3c7126fc8011f6927359b2b6b0d40aa9bcff
vimal336 <asvimal336@gmail.com>
2023-07-17 16:45:05 +0530

    node js practice

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const math = require('./math')
+
+const {add, sub, mul, div} = require('./math')
+
+console.log(add(3,3))
+console.log(sub(3,3))
+console.log(mul(3,3))
+console.log(div(3,3))
+
 
 //OS Methods:
 var os = require('os');
@@ -9,12 +18,16 @@ console.log(os.version());
 
 
 console.log(__dirname);
+
 console.log(__filename);
 
 
 const path = require('path');
-console.log(path.sep);
+console.log(path.sep);// \
 
-console.log(path.basename(__filename));
-console.log(path.dirname(__dirname));
-console.log(path.extname(__filename));
+console.log(path.basename(__filename));//index.js  
+console.log(path.dirname(__dirname));//D:\fabevy
+console.log(path.extname(__filename));//.js
+
+
+
