095fe2540a9237b842d00c502cb3696c1fbe9130
vimal336 <asvimal336@gmail.com>
2023-07-19 14:14:01 +0530

    write and append a file

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 
-// read a file
+// file system
 const fs = require('fs')
 const path = require('path')
 
 //import { readFile } from 'node:fs';
 
+// read a file
 fs.readFile((path.join(__dirname,'files','hello.txt')), 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data.toString());
+});
+
+// write a file 
+fs.writeFile((path.join(__dirname,'files','write.txt')), 'create a file using node js', (err, data) => {
+  if (err) throw err;
+  console.log('Write complete');
+});
+
+
+// Modify file (e.g., append some text)
+  
+fs.appendFile((path.join(__dirname,'files','write.txt')), '\n\n mofify or update a file using node js', (err, data) => {
+  if (err) throw err;
+  console.log('update complete');
 });
 
 //Exit on uncaught errors 
