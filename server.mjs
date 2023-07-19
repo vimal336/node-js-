c9b5d311924afe002ba1859d8e586012be635252
vimal336 <asvimal336@gmail.com>
2023-07-19 13:00:39 +0530

    exit process

--- server.mjs
+++ server.mjs
@@ -2,7 +2,13 @@
 // read a file
 import { readFile } from 'node:fs';
 
-readFile('files/hello.txt', (err, data) => {
+readFile('files/hello.txt', 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data.toString());
 });
+
+//Exit on uncaught errors 
+process.on('uncaughtException', err => {
+console.error(`There was an uncaught error: ${err}`)
+process.exit(1);
+})
