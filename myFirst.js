23388c3f01750ee093337238ef3dae908da5d652
vimal336 <asvimal336@gmail.com>
2023-07-23 22:39:44 +0530

    node js eventEmitter object

--- myFirst.js
+++ myFirst.js
@@ -1,9 +1,27 @@
 var http = require('http');
+var dt = require('./dateModule')
 
 http.createServer(function (req, res) {
   console.log('response send')
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+  res.write('The date and time is' +dt.myDateTime())
+  res.end()
+}).listen(8000);
 
 //http://localhost:8080/
+
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var myEventHandler = function () {
+  console.log('I hear a scream!');
+}
+
+//Assign the eventhandler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
+
+              
