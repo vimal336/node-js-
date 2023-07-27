c7c169176f921921dcee718157022c3e4bcb20ce
vimal336 <asvimal336@gmail.com>
2023-07-27 22:36:40 +0530

    node uuid

--- dateModule.js
+++ dateModule.js
@@ -4,4 +4,9 @@ exports.myDateTime = () => {
 
 const { format } = require('date-fns');
 
+const {v4: uuid} = require('uuid')
+
 console.log(format(new Date(), 'dd-MM-yyyy\tHH:mm:ss'))
+
+console.log(uuid());
+
