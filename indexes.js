2712c1f16d38a40dc2cf4e70179594540d6e4e43
vimal336 <asvimal336@gmail.com>
2023-08-02 21:04:22 +0530

    export arihmetic operation

---
+++ indexes.js
@@ -0,0 +1,31 @@
+
+const {add, sub, mul, div} = require('./math')
+
+console.log(add(3,3))
+console.log(sub(3,3))
+console.log(mul(3,3))
+console.log(div(3,3))
+
+
+// OS Methods:
+// var os = require('os');
+
+// console.log(os.uptime());
+// console.log(os.type());
+// console.log(os.homedir());
+// console.log(os.version());
+
+
+// console.log(__dirname);
+
+// console.log(__filename);
+
+// const path = require('path');
+// console.log(path.sep);// \
+
+// console.log(path.basename(__filename));//index.js  
+// console.log(path.dirname(__dirname));//D:\fabevy
+// console.log(path.extname(__filename));//.js
+
+
+
