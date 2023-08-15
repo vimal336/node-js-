a7e3f6c85a357fd55a0954840b8ad153c8c6bd92
vimal336 <asvimal336@gmail.com>
2023-08-15 19:21:14 +0530

    check callback error

--- NodeGoodPractice/cbError.js
+++ NodeGoodPractice/cbError.js
@@ -1,2 +1,8 @@
- Always Check for "error" in Callbacks
+// Always Check for "error" in Callbacks
 // As stated above, by convention an error is always the first parameter passed to any callback function. This is great for making sure your site doesn't crash and that you can detect errors when they happen.
+
+// Now that you know what they are you should start using them. If your database query errors out you need to check for that before using the results. I'll give you an example.
+
+myAsyncFunction({
+    some: 'data'
+}, function(err, someReturnedData) {
