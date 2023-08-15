67104feb5ed797aa8c4a8bc27a8212d52dc6403d
vimal336 <asvimal336@gmail.com>
2023-08-15 19:20:38 +0530

    check callback error

---
+++ NodeGoodPractice/cbError.js
@@ -0,0 +1,2 @@
+ Always Check for "error" in Callbacks
+// As stated above, by convention an error is always the first parameter passed to any callback function. This is great for making sure your site doesn't crash and that you can detect errors when they happen.
