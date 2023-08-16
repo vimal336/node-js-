d8fb0e9ebaa84a9e9230eded328681a61123cd6e
vimal336 <asvimal336@gmail.com>
2023-08-16 15:33:28 +0530

    use exception handling when errors can be thrown

---
+++ NodeGoodPractice/exceptionalHandling.js
@@ -0,0 +1,45 @@
+// Use Exception Handling When Errors Can Be Thrown
+
+// Most methods in Node.js will follow the "error first" convention, but some functions don't. 
+// These functions are not Node.js specific function, 
+// they instead come from JavaScript. 
+// There are lots of functions that can cause exceptions. 
+// One of these functions is JSON.parse which throws an error if it can't parse a string into JSON.
+
+// How do we detect this error without crashing our server?
+
+// This is a perfect time to use a classic JavaScript try catch.
+
+var parsedJSON;
+
+try {
+	parsedJSON = JSON.parse('some invalid JSON');
+} catch (err) {
+	// do something with your error
+}
+
+if (parsedJSON) {
+	// use parsedJSON
+}
+// You can now be sure that the JSON was parsed correctly before using it.
+
+// This can be even more useful when using it in modules.
+
+function parseJSON(stringToParse, callback) {
+
+	callback = (typeof callback === 'function') ? callback : function() {};
+
+	try {
+
+		var parsedJSON = JSON.parse(stringToParse);
+
+		callback(null, parsedJSON);
+
+	} catch (err) {
+
+		callback(err, null);
+
+	}
+
+}
+// Of course the above example is slightly contrived, however, the idea of using try catch is a very good practice.
