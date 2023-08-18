92ac8abc7ec6be8a8eab16da5a8eef00042c0f5c
vimal336 <asvimal336@gmail.com>
2023-08-18 19:11:24 +0530

    use module.export not exports

--- NodeGoodPractice/useModules.js
+++ NodeGoodPractice/useModules.js
@@ -7,3 +7,12 @@ So what's the problem? That sounds great.
 Well don't get too excited. exports only collects properties and attaches them if module.exports doesn't already have existing properties. If module.exports has any properties, everything attached to exports is ignored and not attached to module.exports.
 
 module.exports = {};
+
+exports.someProperty = 'someValue';
+someProperty won't be exported as part of the module.
+
+var exportedObject = require('./mod');
+
+console.log(exportedObject); // {}
+The solution is simple. Don't use exports because it can create confusing, hard to track down bugs.
+
