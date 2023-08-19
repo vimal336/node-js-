c2905106463de1d92dee90fdaf3577468110d567
vimal336 <asvimal336@gmail.com>
2023-08-19 22:14:03 +0530

    use module.export not exports

--- NodeGoodPractice/useModules.js
+++ NodeGoodPractice/useModules.js
@@ -1,26 +1,28 @@
-Use module.exports not exports
+//Use module.exports not exports
 
-You might have used module.exports and exports interchangeably thinking they are the same thing and in many cases they are. However, exports is more of a helper method that collects properties and attaches them to module.exports.
+// You might have used module.exports and exports interchangeably thinking they are the same thing and in many cases they are. However, exports is more of a helper method that collects properties and attaches them to module.exports.
 
-So what's the problem? That sounds great.
+// So what's the problem? That sounds great.
 
-Well don't get too excited. exports only collects properties and attaches them if module.exports doesn't already have existing properties. If module.exports has any properties, everything attached to exports is ignored and not attached to module.exports.
+// Well don't get too excited. exports only collects properties and attaches them if module.exports doesn't already have existing properties. If module.exports has any properties, everything attached to exports is ignored and not attached to module.exports.
 
 module.exports = {};
 
 exports.someProperty = 'someValue';
-someProperty won't be exported as part of the module.
+
+//someProperty won't be exported as part of the module.
 
 var exportedObject = require('./mod');
 
 console.log(exportedObject); // {}
-The solution is simple. Don't use exports because it can create confusing, hard to track down bugs.
+
+//The solution is simple. Don't use exports because it can create confusing, hard to track down bugs.
 
 module.exports = {};
 
 module.exports.someProperty = 'someValue';
-someProperty will be exported as part of the module.
+
+//someProperty will be exported as part of the module.
 
 var exportedObject = require('./mod');
 
-console.log(exportedObject); // { someProperty: 'someValue' }
