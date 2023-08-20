3924d13714e282e8ef71936cf8577b7006faf399
vimal336 <asvimal336@gmail.com>
2023-08-20 21:08:14 +0530

    this keyword

--- NodeGoodPractice/thisKeyword.js
+++ NodeGoodPractice/thisKeyword.js
@@ -1,3 +1,6 @@
 // Save a reference to this Because it Changes Based on Context
 // If you have a background with Java, ActionScript, PHP, or basically any language that uses the this keyword you might think you understand how JavaScript treats the same keyword. Unfortunately you would be wrong.
 
+// Let me tell you how this is determined officially by ECMAScript.
+
+// The this keyword evaluates to the value of the ThisBinding of the current execution context.
