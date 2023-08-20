b9370f0a7dc547d4f0e98f1872f47d288011be09
vimal336 <asvimal336@gmail.com>
2023-08-20 21:06:53 +0530

    this keyword

---
+++ NodeGoodPractice/thisKeyword.js
@@ -0,0 +1,3 @@
+Save a reference to this Because it Changes Based on Context
+If you have a background with Java, ActionScript, PHP, or basically any language that uses the this keyword you might think you understand how JavaScript treats the same keyword. Unfortunately you would be wrong.
+
