9d56368df4447be49b07efd202e166bdad1ff0d8
vimal336 <asvimal336@gmail.com>
2024-03-01 19:55:33 +0530

    js problem solving

--- jsPrac/prac.js
+++ jsPrac/prac.js
@@ -10,4 +10,4 @@ console.log(findWordLength("hello javascript"));
 function isPalindrome(str){
  return str === str.split('').reverse().join('');
 }
-console.log(isPalindrome("madam"));
+console.log(isPalindrome("hello"));
