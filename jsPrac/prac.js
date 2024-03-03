c5c25711a614ebf06d5d5dca720c1bceaf362066
vimal336 <asvimal336@gmail.com>
2024-03-03 17:27:16 +0530

    ja practice problem

--- jsPrac/prac.js
+++ jsPrac/prac.js
@@ -1,20 +1,24 @@
-
-function findWordLength(stri){
- return stri.split(/\s+/).length;
+function findWordLength(stri) {
+  return stri.split(/\s+/).length;
 }
 
 console.log(findWordLength("hello javascript"));
 
 //check a word is palindrome or not
 
-function isPalindrome(str){
- return str === str.split('').reverse().join('');
+function isPalindrome(str) {
+  return str === str.split("").reverse().join("");
 }
 console.log(isPalindrome("malayalam"));
 
-
 //fibonnacci series
 
-let a = 0, b = 1, c;
+let a = 0,
+  b = 1,
+  c;
 
 let num = 10;
+
+for(let i<=10 i<=num i++){
+
+}
