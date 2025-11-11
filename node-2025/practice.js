7691e27f2f20f9f6bd9f6dba865a563ef502bf24
vimal336 <asvimal336@gmail.com>
2025-11-11 18:20:18 +0530

    some init msg

--- node-2025/practice.js
+++ node-2025/practice.js
@@ -4,8 +4,16 @@ console.log("node js practice");
 // to get file system
 const fs = require("fs");
 
-fs.writeFileSync("test.txt", "hello node.js 2025");
 
 
+// create text file(Synchronous)
+// fs.writeFileSync("test.txt", "hello node.js 2025");
 
+// create text file in particular location
+// fs.writeFileSync("../intro/test.txt", "hello node.js 2025");
 
+// console.log("file written")
+
+// Read file(Synchronous)
+const data = fs.readFileSync('test.txt', { encoding: "utf-8"});
+console.log("Read synchronously", data)
