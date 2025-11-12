bbca988e1043441df26e9ced3408ab011cb1bfd2
vimal336 <asvimal336@gmail.com>
2025-11-12 11:23:26 +0530

    some init msg

--- node-2025/practice.js
+++ node-2025/practice.js
@@ -6,14 +6,40 @@ const fs = require("fs");
 
 
 
-// create text file(Synchronous)
+// Write text file(Synchronous)
+
 // fs.writeFileSync("test.txt", "hello node.js 2025");
 
-// create text file in particular location
+// Write text file(Synchronous) in particular location
+
 // fs.writeFileSync("../intro/test.txt", "hello node.js 2025");
 
 // console.log("file written")
 
-// Read file(Synchronous)
-const data = fs.readFileSync('test.txt', { encoding: "utf-8"});
+
+//Read file(Synchronous) without encoding
+
+const data = fs.readFileSync('test.txt');
 console.log("Read synchronously", data)
+
+//Read file(Synchronous) with encoding
+
+// const encodeddata = fs.readFileSync('test.txt', { encoding: "utf-8"});
+// console.log("Read synchronously", encodeddata)
+
+
+// Write text file(ASynchronous)
+
+fs.writeFile("asyncTest.txt", "async file from nodejs", (err)=>{
+    if(err) throw err
+    console.log("async file created")
+
+    // Read file (ASynchronous)
+    fs.readFile("asyncTest.txt",{ encoding : "utf-8"}, (err, data) =>{
+        console.log("Read file Async", data)
+    } )
+})
+
+console.log("async file creating")
+
+
