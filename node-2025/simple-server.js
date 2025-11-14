b3ebac89ffbb48cf59064ca1ad58d51c63f33c42
vimal336 <asvimal336@gmail.com>
2025-11-14 22:30:18 +0530

    server file

--- node-2025/simple-server.js
+++ node-2025/simple-server.js
@@ -1,35 +1,45 @@
 
 const http = require('http');
 
+const fs = require('fs');
+
 //Create server
 // content type plain text
 
-const server = http.createServer((req, res)=>{
-res.writeHead(200, { "content-type": "text/plain"});
-res.end("Hello backend is working")
-});
+// const server = http.createServer((req, res)=>{
+// res.writeHead(200, { "content-type": "text/plain"});
+// res.end("Hello backend is working")
+// });
 
 // content type json
-const server2 = http.createServer((req, res)=>{
-res.writeHead(200, { "content-type": "application/json"});
-res.end(`{"name":"vimal"}`)
-});
+// const server2 = http.createServer((req, res)=>{
+// res.writeHead(200, { "content-type": "application/json"});
+// res.end(`{"name":"vimal"}`)
+// });
 
 
 // Server with route
-const server3 = http.createServer((req, res)=>{
+const server = http.createServer((req, res) => {
 
- if(req.url == "/"){
-res.writeHead(200, { "content-type": "application/json"});
-res.end(`{"name":"Home page"}`)
- } else if(req.url == "/about"){
-res.writeHead(200, { "content-type": "application/json"});
-res.end(`{"name":"About Page"}`)
- }  
+    if (req.url == "/") {
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(`{"name":"Home page"}`)
+    } else if (req.url == "/about") {
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(`{"name":"About Page"}`)
+    }
+    else if (req.url == "/file") {
+        const readStream = fs.createReadStream("./sample.mp4")
+        res.writeHead(200, { "content-type": "video/mp4" });
+        readStream.pipe(res);
+    } else {
+        res.writeHead(404, { "content-type": "application/json" });
+        res.end(`{"name":"404 Page NotFound"}`)
+    }
 });
 
 // Start a server listening for connections.
-server.listen(5000, ()=>{
+server.listen(5000, () => {
     console.log("server listening on http://localhost:5000")
 })
 
