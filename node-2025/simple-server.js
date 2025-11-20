09bc7688236068bf7b841c88ad94dec3bba28bdf
vimal336 <asvimal336@gmail.com>
2025-11-20 15:36:50 +0530

    server file

--- node-2025/simple-server.js
+++ node-2025/simple-server.js
@@ -34,6 +34,7 @@ const server = http.createServer((req, res) => {
         readStream.pipe(res);
      else if (req.url == "/images") {
          const readStream = fs.createReadStream("./sample.mp3")
+           res.
     } else {
         res.writeHead(404, { "content-type": "text/plain" });
         res.end(`{"name":"404 Page Not found"}`)
